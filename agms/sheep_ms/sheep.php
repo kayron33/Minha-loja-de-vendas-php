67e37831c788ea4f4fbea8e6b5ba69490a7a638d
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-06-18 12:52:41 -0300

    ok

--- agms/sheep_ms/sheep.php
+++ agms/sheep_ms/sheep.php
@@ -1,24 +1,6 @@
           <?php
           
-              /**********************************************************************
-             * ********************************************************************
-             * PAINEL SHEEP - MAYKONSILVEIRA.COM.BR E MAYKON SILVEIRA
-             * MSFLIX.COM.BR 
-             * 
-             * ********************************************************************
-             * MAYKONSILVEIRA.COM.BR DEREICIONANDO VOCÊ PARA O CAMINHO DO SUCESSO #*
-             * *************MAYKON***SILVEIRA**************************************
-             * *************sheep**TECHNOLOGIES***********************************
-             * ********************************************************************
-             * TUDO AQUI FOI CRIADO NO DIA 01-05-2023 POR MAYKON SILVEIRA
-             * TODOS OS DIREITOS RESERVADOS E CÓDIGO FONTE RASTREADO COM ARQUIVOS 
-             * CRIADO POR MAYKONSILVEIRA.COM.BR DESDE 2007 *********
-             * TODA SABEDORIA PARA CRIAR ESTES SISTEMAS VEM DO SANTO E ETERNOR PAI
-             * O SANTO SENHOR DEUS DE ABRAÃO, ISSAC E JACÓ E DO MEU ÚNICO SENHOR 
-             * O MESSIAS NOSSO SALVADOR, POIS A GLORIA É DO PAI E DO FILHO PARA SEMPRE
-             * ********************************************************************
-             */
-             // topo Maykon Silveira
+             
              require_once('sheep_topo.php');
             
              $sheep_caminho_painel = '';
